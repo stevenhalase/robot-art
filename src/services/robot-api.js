3d182363cc5828d165d9e548b4b94ec067259bb2
Steven Halase <steven.halase@gmail.com>
2021-06-01 12:44:54 -0600

    Adding server for Heroku

--- src/services/robot-api.js
+++ src/services/robot-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/';
+const BASE_URL = 'https://robot-art-api.herokuapp.com/';
 
 class RobotAPIService {
 	static async login(email, password) {
